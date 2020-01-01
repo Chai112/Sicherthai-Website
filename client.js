17f1477cbe30e054aea6bfade6d216a93df76c63
Chai <sirdesignpie@gmail.com>
2020-01-01 17:48:11 +0700

    Reformat client.js

--- client.js
+++ client.js
@@ -4,19 +4,19 @@
 var url = "http://192.168.1.107:80/test/server.php?id=%22hello%22/";
 
 function run() {
-    document.getElementById("out").innerHTML = "STAND BY (1/2)";
+    $("#out").text("STAND BY (1/2)");
     $.getJSON(
         url,
         function(data) {
-        
-        var text = `City: ${data.city}<br>
-                    ID: ${data.id}<br>`
-                    
-        
-        $("#out").text("ALL GO (2/2) " + text);
-    });
+
+            var text = `City: ${data.city}<br>
+                        ID: ${data.id}<br>`
+            
+            $("#out").text("ALL GO (2/2) " + text);
+        }
+    );
 }
 
 // We'll run the AJAX query when the page loads.
-document.getElementById("out").innerHTML = "STAND BY (0/2)";
+$("#out").text("STAND BY (0/2)");
 window.onload=run;
