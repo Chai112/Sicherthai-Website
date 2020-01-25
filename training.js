e03adfbbf9e4c4b503ed7c9ab301cd53ec048d5f
Chai <sirdesignpie@gmail.com>
2020-01-25 21:17:28 +0700

    Add training

---
+++ training.js
@@ -0,0 +1,67 @@
+var url = "https://sicher.000webhostapp.com/?id=";
+var id = "aaa";
+var q = "q";
+
+url = url + id;
+url = url + "&q="
+url = url + q;
+
+var sep = 0;
+document.getElementById("out").style="display:none";
+//var url = "http://validate.jsontest.com/?json=a";
+
+function report(item, index)
+{
+    item.name = item.name.replace(/_/g, " ");
+
+    $("#out-title").text(item.name);
+    $("#out-txt").text("a");
+    var out = document.getElementById("out").cloneNode(true);
+    out.style="max-width:50%;height:10vh;margin-top:" + sep + "em";
+    list.style="margin-bottom:" + (sep + 11) + "em";
+    sep = sep + 7;
+    document.getElementById("list").appendChild(out);
+
+    if (item.is_public === "1")
+    {
+        //document.write("public training<br>");
+    }
+    else
+    {
+        //document.write("in house training<br>");
+    }
+
+    /*document.write("</b>" +
+        "date: " + item.date + "<br>" +
+        "duration: " + item.duration + "<br>" +
+        "price: " + item.price + "<br>" +
+        "location: " + item.location + "<br>" +
+        "description: " + item.description + "<br>" +
+        "</p>"
+    );*/
+}
+
+function run() {
+    $.getJSON(
+        url,
+        function(data) {
+            var text = data.status;
+            if (data.err)
+            {
+                text = "SERVER ERROR: " + text;
+                alert(text);
+            }
+            if (id !== data.id)
+            {
+                text = "AUTH ERROR: IDs do not match";
+            }
+
+            //$("#out").text(text);
+            data.answer.forEach(report);
+        }
+    );
+}
+
+// We'll run the AJAX query when the page loads.
+//$("#out").text("Connecting...");
+window.onload=run;
