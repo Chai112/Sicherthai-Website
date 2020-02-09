4489701a6197d9be0bbeb0fd907bc74cadb79174
Chai <sirdesignpie@gmail.com>
2020-02-09 18:07:45 +0700

    Add

--- t-page.js
+++ t-page.js
@@ -7,26 +7,28 @@ url = url + "&q="
 url = url + q;
 
 var sep = 0;
-document.getElementById("out").style="display:none";
 //var url = "http://validate.jsontest.com/?json=a";
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
 
 function report(item, index)
 {
     item.name = item.name.replace(/_/g, " ");
-    $("#out-title").text(item.name);
-    $("#out-txt").text("location: " + item.location + " date: " + item.date);
-    var out = document.getElementById("out").cloneNode(true);
-    out.style="padding-left:3vw;max-width:70em;height:15vh;margin-top:" + sep + "em";
 
-    w = window.location.href;
-    i = window.location.href.length;
-    while (window.location.href[i] != '/')
-    {
-        i--;
-    }
-    $(out).click(function() {window.open(window.location.href.substring(0, i) + "/t-page.html?p=" + item.name);});
-    list.style="margin-bottom:" + (sep + 10) + "em";
-    sep = sep + 10;
+    $("#out-title").text(item.name);
+    $("#out-txt").text("a");
+    var out = document.getElementById("out").cloneNode(true);
+    out.style="max-width:50%;height:10vh;margin-top:" + sep + "em";
+    list.style="margin-bottom:" + (sep + 11) + "em";
+    sep = sep + 7;
     document.getElementById("list").appendChild(out);
 
     if (item.is_public === "1")
@@ -48,11 +50,8 @@ function report(item, index)
     );*/
 }
 
-$( "#out" ).click(function() {
-  alert( "Handler for .click() called." );
-});
-
 function run() {
+    //alert(getParameterByName("p"));
     $.getJSON(
         url,
         function(data) {
